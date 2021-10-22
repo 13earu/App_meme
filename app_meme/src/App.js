1db35e4afcde50f2ccce18dbb9f8849be3d26b9f
Equipo <13earu@gmail.com>
2021-10-21 22:04:26 -0500

    primer commit

---
+++ app_meme/src/App.js
@@ -0,0 +1,66 @@
+import { useState } from 'react';
+import html2canvas from 'html2canvas';
+import './App.css';
+
+function App() {
+
+
+  const [linea1, setLinea1] = useState('');
+  const [linea2, setLinea2] = useState('');
+  const [imagen, setImagen] = useState('');
+
+  const onChangeLinea1 = function (evento) {  
+    setLinea1(evento.target.value)
+  }
+
+  const onChangeLinea2 = function (evento) {  
+    setLinea2(evento.target.value)
+  }
+
+  const onChangeImagen = function (evento) {  
+    setImagen(evento.target.value)
+  }
+
+const onClickExportar = function (evento) {  
+  html2canvas(document.querySelector("#meme")).then(canvas => {
+    var img = canvas.toDataURL("image/png");
+    
+    var link = document.createElement('a');
+    link.download = 'meme.png'
+    link.href = img;
+    link.click();
+  });
+}
+
+
+  return (
+    <div className="App">
+    
+    <select onChange={onChangeImagen} placeholder="Escoje tu meme1">
+    <option value="" disabled selected hidden>Escoje tu meme</option>
+      <option value="fire">Casa en llamas</option>
+      <option value="futurama">Futurama</option>
+      <option value="history">History Channel</option>
+      <option value="matrix">Matrix</option>
+      <option value="philosoraptor">Philosoraptor</option>
+      <option value="smart">Smart Guy</option>
+      <option value="hola">Hola</option>
+    </select> <br />
+
+    <input onChange={onChangeLinea1} type="text" placeholder="Escribe"></input> <br />
+    <input onChange={onChangeLinea2} type="text" placeholder="Escribe"></input> <br />
+    <button onClick={onClickExportar}>Exportar</button>
+
+    <div className="meme" id="meme">
+      <span>{linea1}</span> <br />
+      <span>{linea2}</span> 
+      <img src={"/img/"+ imagen +".jpg"} />
+    </div>
+
+    </div>
+  );
+}
+
+export default App;
+
+
